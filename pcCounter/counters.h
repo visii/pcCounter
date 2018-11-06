b5db4bc020f877c8726fc6957809eeacc0a56379
Visii <visiiiix@gmail.com>
2018-11-06 11:19:13 -0200

    Add project files.

---
+++ pcCounter/counters.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef COUNTERS_H
+#define COUNTERS_H
+
+int counter(int x);
+int counter1(int x);
+int counter2(int x);
+int counter3(int x);
+int counter4(int x);
+int counter5(int x);
+int counter6(int x);
+int counter7(int x);
+int counter8(int x);
+int counter9(int x);
+int counter10(int x);
+void printCount(int x);
+
+#endif
